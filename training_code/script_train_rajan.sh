38de344902d337356800c9f6f09d24c909099225
Hakjun Moon <gloriel621@gmail.com>
2025-09-08 17:18:24 +0900

    update train files

--- training_code/script_train_rajan.sh
+++ training_code/script_train_rajan.sh
@@ -1,6 +1,6 @@
 #DATASET=path to dataset
 # DATASET="ENTER DATASET PATH"
-EXP_NAME="rajan-reimplement-cocolsun-10k_originalreconimage"
+EXP_NAME="rajan-reimplement-180k-0908"
 #LDM_DS_NAME=directory of checkpoints
 
 # Train on 1000 real and 1000 fake in the sync mode (use half the usual batch size as the code picks a fake image for every real image)
@@ -9,17 +9,17 @@ EXP_NAME="rajan-reimplement-cocolsun-10k_originalreconimage"
 # Train on whole dataset without sync mode
 #CUDA_VISIBLE_DEVICES=2 python train.py --name=$LDM_DS_NAME --arch res50nodown --cropSize 96  --norm_type resnet --resize_size 256 --resize_ratio 0.75 --blur_sig 0.0,3.0 --cmp_method cv2,pil --cmp_qual 30,100 --resize_prob 0.2 --jitter_prob  0.8 --colordist_prob 0.2 --cutout_prob 0.2 --noise_prob 0.2 --blur_prob 0.5 --cmp_prob 0.5 --rot90_prob 1.0 --dataroot $DATASET --batch_size 128 --earlystop_epoch 10 --use_inversions --seed 17 
 
-REAL_DIR="/media/NAS/USERS/gloriel621/DF_dataset/af_trainset/real/"
-FAKE_DIR="/media/NAS/USERS/gloriel621/DF_dataset/af_trainset/fake/"
+REAL_DIR="/media/NAS/DATASET/COCO/af_trainset_180k/real/"
+FAKE_DIR="/media/NAS/DATASET/COCO/af_trainset_180k/fake/"
 
 # ICML experiments (Stay-Positive with batch level alignment)
-CUDA_VISIBLE_DEVICES=4 python train.py \
+CUDA_VISIBLE_DEVICES=1 python train.py \
     --name=$EXP_NAME \
     --arch res50nodown \
-    --data_cap=10000 \
+    --data_cap=180000 \
     --real_dir $REAL_DIR \
     --fake_dir $FAKE_DIR \
-    --val_split 0.2 \
+    --val_split 0.1 \
     --cropSize 96  \
     --norm_type resnet \
     --resize_size 256 \
@@ -40,5 +40,6 @@ CUDA_VISIBLE_DEVICES=4 python train.py \
     --seed 14 \
     --final_dropout 0.5 \
     --use_inversions \
-    --batched_syncing 
+    --batched_syncing \
+    --gradient_accumulation_steps 2
 
--- training_code/script_train_staypos.sh
+++ training_code/script_train_staypos.sh
@@ -1,6 +1,6 @@
 #DATASET=path to dataset
 # DATASET="ENTER DATASET PATH"
-EXP_NAME="staypos-reimplement-cocolsun-10k_originalreconimage"
+EXP_NAME="staypos-reimplement-cocolsun-180k"
 #LDM_DS_NAME=directory of checkpoints
 
 # Train on 1000 real and 1000 fake in the sync mode (use half the usual batch size as the code picks a fake image for every real image)
@@ -9,18 +9,18 @@ EXP_NAME="staypos-reimplement-cocolsun-10k_originalreconimage"
 # Train on whole dataset without sync mode
 #CUDA_VISIBLE_DEVICES=2 python train.py --name=$LDM_DS_NAME --arch res50nodown --cropSize 96  --norm_type resnet --resize_size 256 --resize_ratio 0.75 --blur_sig 0.0,3.0 --cmp_method cv2,pil --cmp_qual 30,100 --resize_prob 0.2 --jitter_prob  0.8 --colordist_prob 0.2 --cutout_prob 0.2 --noise_prob 0.2 --blur_prob 0.5 --cmp_prob 0.5 --rot90_prob 1.0 --dataroot $DATASET --batch_size 128 --earlystop_epoch 10 --use_inversions --seed 17 
 
-REAL_DIR="/media/NAS/USERS/gloriel621/DF_dataset/af_trainset/real/"
-FAKE_DIR="/media/NAS/USERS/gloriel621/DF_dataset/af_trainset/fake/"
+REAL_DIR="/media/NAS/USERS/gloriel621/DF_dataset/af_trainset_180k/real/"
+FAKE_DIR="/media/NAS/USERS/gloriel621/DF_dataset/af_trainset_180k/fake/"
 
 # ICML experiments (Stay-Positive with batch level alignment)
 CUDA_VISIBLE_DEVICES=1 python train.py \
     --name=$EXP_NAME \
     --arch res50nodown \
-    --data_cap=10000 \
+    --data_cap=200000 \
     --real_dir $REAL_DIR \
     --fake_dir $FAKE_DIR \
-    --val_split 0.2 \
-    --cropSize 96  \
+    --val_split 0.1 \
+    --cropSize 96 \
     --norm_type resnet \
     --resize_size 256 \
     --resize_ratio 0.75 \
@@ -36,11 +36,12 @@ CUDA_VISIBLE_DEVICES=1 python train.py \
     --cmp_prob 0.5 \
     --rot90_prob 1.0 \
     --batch_size 32 \
-    --earlystop_epoch 10 \
+    --earlystop_epoch 5 \
     --seed 14 \
     --stay_positive "clamp"\
-    --fix_backbone\
-    --final_dropout 0.0\
-    --use_inversions\
-    --batched_syncing 
+    --fix_backbone \
+    --final_dropout 0.0 \
+    --use_inversions \
+    --batched_syncing \
+    --ckpt /media/NAS/USERS/gloriel621/DF_methods/AlignedForensics/test_code/weights/ours-sync/weights.pth \
 
